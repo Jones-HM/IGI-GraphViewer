713c1572651ece1190e010cf4fedd5fb5de7d5c0
Haseeb <haseeb_heaven@yahoo.com>
2023-08-12 12:20:46 +0400

    Added Graph 3D Gen

---
+++ graph_data_parser.py
@@ -0,0 +1,164 @@
+import logging
+import struct
+import json
+import tkinter as tk
+from tkinter import filedialog, Text, Button
+from graph_const import material_mapping
+
+class GraphData:
+    def __init__(self, name, hex_bytes, data_type, size):
+        self.name = name
+        self.hex_bytes = hex_bytes.replace(" ", "").lower()
+        self.data_type = data_type
+        self.size = size
+        self.data = []
+        
+    def clear_data(self):
+        self.data = []
+
+    def extract_data(self, binary_data, start_index):
+        index = binary_data.find(bytes.fromhex(self.hex_bytes), start_index)  # Search for the full 4-byte hex signature
+        if index == -1:
+            logging.info(f"Hex signature {self.hex_bytes} not found after index {start_index}.")
+            return start_index  # If not found, return the same index
+        data_start_index = index + 8  # Adjusting for the 4 bytes offset
+        end_index = data_start_index + self.size
+
+        raw_data = binary_data[data_start_index:end_index]
+        if self.data_type == "Stringx18":
+            cleaned_data = ''.join([char if 32 <= ord(char) < 127 else '' for char in raw_data.decode(errors='ignore')]).strip()
+            self.data.append(cleaned_data)
+        elif self.data_type == "Real64x3":
+            x, y, z = struct.unpack('ddd', raw_data)
+            self.data.append((round(x, 2), round(y, 2), round(z, 2)))
+        else:
+            self.data.append(raw_data)
+        
+        logging.info(f"Extracted data for {self.name} at index {index}. Data: {self.data[-1]}")
+        return end_index
+
+# graph file structure
+graphDatList = [
+    GraphData("Max Nodes", "04 E6 3A 0D", "Integer", 4),
+    GraphData("Node Id", "04 CE 35 07", "Integer", 4),
+    GraphData("Node Position", "04 95 42 1D", "Real64x3", 24),
+    GraphData("Node Gamma", "04 9C 7E 0F", "Single", 4),
+    GraphData("Node Radius", "04 23 30 14", "Single", 4),
+    GraphData("Node Material", "04 29 B6 1B", "Integer", 1),
+    GraphData("Node Criteria", "04 E5 D3 1B", "Stringx18", 20),
+    GraphData("Graph Edge1", "04 4A 10 09", "Integer", 2),
+    GraphData("Graph Edge2", "04 F6 18 09", "Integer", 2),
+    GraphData("Graph EdgeType", "04 23 A9 0D", "Integer", 1)
+]
+
+def convert_to_json(graphDataList):
+    nodes = []
+    edges = list(zip(graphDataList[7].data, graphDataList[8].data, graphDataList[9].data))  # Combine edge1, edge2, and type
+
+    node_counter = 1  # Counter for node IDs
+
+    for i in range(len(graphDataList[1].data)):
+        # Use the node_counter for the node ID
+        node_id = node_counter
+        node_counter += 1
+        
+        # Filter edges that are directly connected to the current node
+        connected_edges = []
+        for edge in edges:
+            edge_id_1 = int.from_bytes(edge[0], "little", signed=False)
+            edge_id_2 = int.from_bytes(edge[1], "little", signed=False)
+            if node_id == edge_id_1:
+                connected_edges.append(edge_id_2)
+            elif node_id == edge_id_2:
+                connected_edges.append(edge_id_1)
+        
+        material_id = int(graphDataList[5].data[i].hex(), 16)
+        material_name = material_mapping.get(material_id, "UNKNOWN")
+        
+        node = {
+            "id": node_id,
+            "x": graphDataList[2].data[i][0],
+            "y": graphDataList[2].data[i][1],
+            "z": graphDataList[2].data[i][2],
+            "gamma": struct.unpack('f', graphDataList[3].data[i])[0],
+            "radius": struct.unpack('f', graphDataList[4].data[i])[0],
+            "material": material_name,
+            "description": graphDataList[6].data[i] if 'nodecriteria'.lower() in graphDataList[6].data[i].lower() else "",
+            "edges": connected_edges
+        }
+        nodes.append(node)
+    return json.dumps(nodes, indent=4)
+
+def select_file(filename):
+    binary_data = read_binary_file(filename)
+    
+    # Clear existing data
+    for graphDat in graphDatList:
+        graphDat.clear_data()
+        
+    if binary_data:
+        extract_node_data(binary_data, graphDatList)
+        extract_edge_data(binary_data, graphDatList)
+        json_data = convert_to_json(graphDatList)
+        
+    return json_data
+
+def setup_logging():
+    logging.basicConfig(filename='graph_data.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+def read_binary_file(filename):
+    try:
+        with open(filename, "rb") as file:
+            return file.read()
+    except Exception as e:
+        logging.error(f"Error reading file {filename}: {e}")
+        return None
+
+def extract_node_data(binary_data, graphDatList):
+    logging.info("Extracting node data...")
+    start_index = 0
+    while start_index < len(binary_data):
+        logging.info(f"Searching for node data at index {start_index}...")
+        node_id_index = binary_data.find(bytes.fromhex("04 CE 35 07"), start_index)
+        
+        if node_id_index == -1:
+            logging.info("No more node data found.")
+            break
+        
+        next_node_id_index = binary_data.find(bytes.fromhex("04 CE 35 07"), node_id_index + 4)
+        if next_node_id_index == -1:
+            next_node_id_index = len(binary_data)
+        
+        for graphDat in graphDatList:
+            if "Edge" not in graphDat.name:  # Skip edge properties for node extraction
+                prop_index = graphDat.extract_data(binary_data, node_id_index)
+                if prop_index >= next_node_id_index:
+                    break
+        
+        start_index = next_node_id_index
+
+
+
+def extract_edge_data(binary_data, graphDatList):
+    logging.info("Extracting edge data...")
+    start_index = 0
+    while start_index < len(binary_data):
+        logging.info(f"Searching for edge data at index {start_index}...")
+        edge_id_index = binary_data.find(bytes.fromhex("04 4A 10 09"), start_index)
+        
+        if edge_id_index == -1:
+            logging.info("No more edge data found.")
+            break
+        
+        for graphDat in graphDatList:
+            if "Edge" in graphDat.name:  # Only process edge properties for edge extraction
+                start_index = graphDat.extract_data(binary_data, edge_id_index)
+        
+def print_results(graphDatList):
+    for graphDat in graphDatList:
+        if graphDat.data_type == "Stringx18":
+            logging.info(f"{graphDat.name}: {graphDat.data}")
+        elif graphDat.data_type == "Real64x3":
+            logging.info(f"{graphDat.name}: [{' '.join(map(str, pos))}]" for pos in graphDat.data)
+        else:
+            logging.info(f"{graphDat.name}: [{' '.join(map(lambda x: x.hex().upper(), graphDat.data))}]")
---
+++ graph_gui.py
@@ -0,0 +1,253 @@
+"""
+This is Project IGI Graph Generator GUI which generate the 3D graph of the game using the graph data file.
+This also has the option to export the graph data to JSON file.
+date : 12 - Aug - 2023
+author : @heaven_hm
+"""
+
+import json
+import logging
+import os
+import tempfile
+import tkinter as tk
+from tkinter import filedialog, ttk, messagebox, font, Scrollbar
+import webbrowser
+import plotly.graph_objects as go
+import tkinter as tk
+from tkinter import ttk
+from tkinter import font
+from tkinter import Scrollbar
+from graph_data_parser import select_file,material_mapping
+from graph_const import material_colors,material_mapping
+from pygments import highlight
+
+# Constants
+NODE_RADIUS_SIZE = 30
+NODE_SYMBOL = 'square'
+
+logging.basicConfig(filename='graph_generator.log', level=logging.DEBUG)
+
+def load_json_data(data_str):
+    try:
+        return json.loads(data_str)
+    except json.JSONDecodeError:
+        logging.error("Invalid JSON data")
+        messagebox.showerror("Error", "Invalid JSON data")
+        return None
+
+def get_edges(data):
+    edge_x = []
+    edge_y = []
+    edge_z = []
+    for item in data:
+        if "edges" in item:
+            for edge in item["edges"]:
+                target_node = next((node for node in data if node["id"] == edge), None)
+                if target_node:
+                    edge_x.extend([item["x"], target_node["x"], None])
+                    edge_y.extend([item["y"], target_node["y"], None])
+                    edge_z.extend([item["z"], target_node["z"], None])
+    return edge_x, edge_y, edge_z
+ 
+def prepare_node_colors_and_sizes(data):
+   
+    logging.debug(f"material_colors list: {material_colors}")
+    NODE_RADIUS_SIZE = int(node_radius_entry.get())
+    
+    colors = []
+    sizes = []
+    for item in data:
+        material = item['material']
+        color = material_colors.get(material_mapping.get(material, 'UNKNOWN'), 'purple')  # Default to 'gray' if material isn't found
+        logging.debug(f"Material: {material}, Color: {color}")
+        colors.append(color)
+        sizes.append(item['radius'] * NODE_RADIUS_SIZE) 
+        logging.debug(f"Radius: {item['radius']}")
+    
+    return colors, sizes
+
+
+def prepare_hover_text(data):
+    text_data = []
+    for item in data:
+        text = f"Node ID: {item['id']}"
+        if show_links.get():
+            text += f"<br>Links: {', '.join(map(str, item['edges']))}"
+        if show_material.get():
+            text += f"<br>Material: {item['material']}"
+        if show_gamma_radius.get():
+            text += f"<br>Gamma: {item['gamma']}<br>Radius: {item['radius']}"
+        if show_criteria.get():
+            text += f"<br>Criteria: {item['description']}"
+        text_data.append(text)
+    return text_data
+
+
+def plot_3d(data, plot_type='scatter',symbol=None):
+    logging.info(f"Generating 3D {plot_type} plot")
+    x_data = [item['x'] for item in data]
+    y_data = [item['y'] for item in data]
+    z_data = [item['z'] for item in data]
+    edge_x, edge_y, edge_z = get_edges(data)
+    hover_texts = prepare_hover_text(data)
+    colors, sizes = prepare_node_colors_and_sizes(data)
+    
+    fig = go.Figure()
+    if plot_type == 'scatter':
+        fig.add_trace(go.Scatter3d(x=x_data, y=y_data, z=z_data, mode='markers', marker=dict(color=colors, size=sizes, symbol=symbol, sizemode='diameter'), text=hover_texts, hoverinfo='text'))
+    elif plot_type == 'surface':
+        fig.add_trace(go.Scatter3d(x=x_data, y=y_data, z=z_data, mode='markers', marker=dict(color=colors, size=sizes, sizemode='diameter'), text=hover_texts, hoverinfo='text'))
+    elif plot_type == 'line':
+        fig.add_trace(go.Scatter3d(x=x_data, y=y_data, z=z_data, mode='lines+markers', marker=dict(color=colors, size=sizes, sizemode='diameter'), line=dict(color='red'), text=hover_texts, hoverinfo='text'))
+    elif plot_type == 'mesh':
+        # Use intensity to map colors to a colorscale
+        intensity = list(range(len(colors)))
+        fig.add_trace(go.Mesh3d(x=x_data, y=y_data, z=z_data, opacity=0.5, hoverinfo='text', hovertext=hover_texts, intensity=intensity, colorscale='Viridis', cmin=0, cmax=len(colors)-1))
+        fig.add_trace(go.Scatter3d(x=x_data, y=y_data, z=z_data, mode='markers', marker=dict(color=colors, size=sizes, sizemode='diameter'), text=hover_texts, hoverinfo='text'))
+    else:
+        logging.error(f"Invalid plot type: {plot_type}")
+        return
+
+    fig.add_trace(go.Scatter3d(x=edge_x, y=edge_y, z=edge_z, mode='lines', line=dict(color='red')))
+    fig.show()
+    logging.info(f"3D {plot_type} plot generated successfully")
+
+def on_select_file():
+    try:
+        file_path = filedialog.askopenfilename(filetypes=[("Graph files", "*.dat"), ("All files", "*.*")])
+        if file_path:
+            json_data = select_file(file_path)
+                    
+            json_input.delete("1.0", tk.END)
+            json_input.insert(tk.END, json_data)
+    except Exception as e:
+        logging.error(f"Error selecting file: {e}")
+        messagebox.showerror("Error", "Error selecting file")
+
+def on_export_to_json():
+    data_str = json_input.get("1.0", tk.END)
+    file_path = filedialog.asksaveasfilename(defaultextension=".json", filetypes=[("JSON files", "*.json"), ("All files", "*.*")])
+    if file_path:
+        with open(file_path, 'w') as file:
+            file.write(data_str)
+        messagebox.showinfo("Success", "Graph exported to JSON successfully!")
+
+def adjust_data_based_on_input(data):
+    ignore_z = ignore_node_height.get()
+    if ignore_z:
+        for item in data:
+            item["z"] = 0  # Set Z position to 0 or any other default value
+    return data
+
+def on_generate_graph():
+    try:
+        data_str = json_input.get("1.0", tk.END)
+        data = json.loads(data_str)
+        if not data:
+            return
+
+        # Adjust data based on user's input
+        data = adjust_data_based_on_input(data)
+
+        graph_type = graph_type_combobox.get()
+
+        if graph_type == "3D Scatter":
+            plot_3d(data, plot_type='scatter',symbol=node_symbol_combobox.get())
+        elif graph_type == "3D Surface":
+            plot_3d(data, plot_type='surface',symbol=node_symbol_combobox.get())
+        elif graph_type == "3D Line":
+            plot_3d(data, plot_type='line',symbol=node_symbol_combobox.get())
+        elif graph_type == "3D Mesh":
+            plot_3d(data, plot_type='mesh',symbol=node_symbol_combobox.get())
+    except Exception as e:
+        logging.error(f"Error generating graph: {e}")
+        messagebox.showerror("Error", "Error generating graph")
+
+# Functions
+def on_quit():
+    app.quit()
+
+def on_help():
+    help_message = "This is a GUI for Project IGI Graph Generator which generate the 3D graph of the game using the graph data file.\nSelect the graph file like graph4019.dat (Binary file) and click on Generate Graph button to generate the 3D graph.\nThis also has the option to export the graph data to JSON file.\n\nAuthor: @heaven_hm\nDate: 12 - Aug - 2023"
+    messagebox.showinfo("Help", help_message)
+
+# Main App
+app = tk.Tk()
+app_name = "IGI 3D Graph Generator - HM"
+app.title(app_name)
+app.geometry("700x600")
+app.resizable(False, True)
+
+# Create a notebook (tabs)
+notebook = ttk.Notebook(app)
+notebook.pack(pady=10, padx=10, expand=True, fill=tk.BOTH)
+
+# Create frames for the tabs
+main_frame = ttk.Frame(notebook)
+settings_frame = ttk.Frame(notebook)
+
+notebook.add(main_frame, text="Main")
+notebook.add(settings_frame, text="Settings")
+
+# Main Tab
+ttk.Label(main_frame, text="JSON Data:").grid(row=0, column=0, sticky=tk.W, pady=5, padx=5)
+
+json_input = tk.Text(main_frame, height=10, width=60, wrap=tk.WORD, font=("Courier New", 14))
+json_input.grid(row=1, column=0, columnspan=4, pady=5, padx=5, sticky=tk.W+tk.E)
+scroll_x = tk.Scrollbar(main_frame, orient=tk.HORIZONTAL, command=json_input.xview)
+scroll_x.grid(row=2, column=0, columnspan=4, sticky=tk.W+tk.E)
+scroll_y = tk.Scrollbar(main_frame, orient=tk.VERTICAL, command=json_input.yview)
+scroll_y.grid(row=1, column=4, sticky=tk.N+tk.S)
+json_input['xscrollcommand'] = scroll_x.set
+json_input['yscrollcommand'] = scroll_y.set
+json_input.tag_configure("json", background="yellow")
+
+ttk.Label(main_frame, text="Graph Type:").grid(row=3, column=0, sticky=tk.W, pady=5, padx=5)
+graph_type_combobox = ttk.Combobox(main_frame, values=["3D Scatter", "3D Surface", "3D Line", "3D Mesh"])
+graph_type_combobox.grid(row=3, column=1, sticky=tk.W, pady=5, padx=5)
+graph_type_combobox.set("3D Scatter")
+
+# Settings Tab
+show_links = tk.BooleanVar()
+show_material = tk.BooleanVar()
+show_gamma_radius = tk.BooleanVar()
+show_criteria = tk.BooleanVar()
+
+ttk.Checkbutton(settings_frame, text="Node Links", variable=show_links).grid(row=0, column=0, sticky=tk.W, padx=5, pady=5)
+ttk.Checkbutton(settings_frame, text="Node Material", variable=show_material).grid(row=0, column=1, sticky=tk.W, padx=5, pady=5)
+ttk.Checkbutton(settings_frame, text="Node Gamma/Radius", variable=show_gamma_radius).grid(row=1, column=0, sticky=tk.W, padx=5, pady=5)
+ttk.Checkbutton(settings_frame, text="Node Criteria", variable=show_criteria).grid(row=1, column=1, sticky=tk.W, padx=5, pady=5)
+
+ignore_node_height = tk.BooleanVar()
+ignore_height_checkbox = ttk.Checkbutton(settings_frame, text="Node Ignore Height", variable=ignore_node_height)
+ignore_height_checkbox.grid(row=2, column=0, sticky=tk.W, pady=5, padx=5)
+
+ttk.Label(settings_frame, text="Node Radius Size:").grid(row=3, column=0, sticky=tk.W, pady=5, padx=5)
+node_radius_entry = tk.Entry(settings_frame)
+node_radius_entry.grid(row=3, column=1, sticky=tk.W, pady=5, padx=5)
+node_radius_entry.insert(0, "50")
+
+ttk.Label(settings_frame, text="Node Symbol:").grid(row=4, column=0, sticky=tk.W, pady=5, padx=5)
+node_symbol_combobox = ttk.Combobox(settings_frame, values=['circle', 'circle-open', 'cross', 'diamond', 'diamond-open', 'square', 'square-open', 'x'])
+node_symbol_combobox.grid(row=4, column=1, sticky=tk.W, pady=5, padx=5)
+node_symbol_combobox.set("circle")
+
+
+# Buttons
+button_frame = ttk.Frame(app)
+button_frame.pack(pady=20)
+
+ttk.Button(button_frame, text="Generate Graph", command=on_generate_graph).grid(row=0, column=0, padx=10)
+ttk.Button(button_frame, text="Select Graph File", command=on_select_file).grid(row=0, column=1, padx=10)
+ttk.Button(button_frame, text="Export to JSON", command=on_export_to_json).grid(row=0, column=2, padx=10)
+ttk.Button(button_frame, text="Help", command=on_help).grid(row=0, column=3, padx=10)
+ttk.Button(button_frame, text="Quit", command=on_quit).grid(row=0, column=4, padx=10)
+
+# Status Bar
+status_bar = ttk.Label(app, text="Ready", relief=tk.SUNKEN, anchor=tk.W)
+status_bar.pack(fill=tk.X)
+
+Settings_data_frame = ttk.Frame(main_frame)
+Settings_data_frame.grid(row=6, column=0, columnspan=4, sticky=tk.W, padx=5, pady=5)
+
+app.mainloop()
