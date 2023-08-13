b1886b2aa4e6dab7e32d21d32c256dd6d9572c91
Haseeb <haseeb_heaven@yahoo.com>
2023-08-13 18:37:37 +0400

    Added Node and Graph area viewer

--- graph_viewer_web_app.py
+++ graph_viewer_web_app.py
@@ -11,8 +11,8 @@ import streamlit as st
 import plotly.graph_objects as go
 import json
 import logging
-from libs.graph_data_parser import select_file
-from libs.graph_area_parser import IGI_Graph_Area
+from libs.graph_data_parser import GraphData,select_file
+from libs.graph_area_parser import GraphArea
 from graph_const import material_colors, material_mapping
 import tempfile
 import pandas as pd
@@ -61,13 +61,13 @@ def prepare_hover_text(data, show_links, show_material, show_gamma_radius, show_
     return text_data
 
 def plot_graph(data):
-    if st.session_state.graph_type == "3D Scatter":
+    if st.session_state.graph_type == "Scatter":
         plot_3d(data, plot_type='scatter', symbol=st.session_state.node_symbol, show_links=st.session_state.show_links, show_material=st.session_state.show_material, show_gamma_radius=st.session_state.show_gamma_radius, show_criteria=st.session_state.show_criteria, show_position=st.session_state.node_position, node_radius_size=st.session_state.node_radius_size, scene_aspectmode=st.session_state.scene_aspectmode)
-    elif st.session_state.graph_type == "3D Surface":
+    elif st.session_state.graph_type == "Surface":
         plot_3d(data, plot_type='surface', symbol=st.session_state.node_symbol, show_links=st.session_state.show_links, show_material=st.session_state.show_material, show_gamma_radius=st.session_state.show_gamma_radius, show_criteria=st.session_state.show_criteria, show_position=st.session_state.node_position, node_radius_size=st.session_state.node_radius_size, scene_aspectmode=st.session_state.scene_aspectmode)
-    elif st.session_state.graph_type == "3D Line":
+    elif st.session_state.graph_type == "Line":
         plot_3d(data, plot_type='line', symbol=st.session_state.node_symbol, show_links=st.session_state.show_links, show_material=st.session_state.show_material, show_gamma_radius=st.session_state.show_gamma_radius, show_criteria=st.session_state.show_criteria, show_position=st.session_state.node_position, node_radius_size=st.session_state.node_radius_size, scene_aspectmode=st.session_state.scene_aspectmode)
-    elif st.session_state.graph_type == "3D Mesh":
+    elif st.session_state.graph_type == "Mesh":
         plot_3d(data, plot_type='mesh', symbol=st.session_state.node_symbol, show_links=st.session_state.show_links, show_material=st.session_state.show_material, show_gamma_radius=st.session_state.show_gamma_radius, show_criteria=st.session_state.show_criteria, show_position=st.session_state.node_position, node_radius_size=st.session_state.node_radius_size, scene_aspectmode=st.session_state.scene_aspectmode)
 
 def plot_3d(data, plot_type='scatter', symbol=None, show_links=False, show_material=False, show_gamma_radius=False, show_criteria=False,show_position=False, node_radius_size=50,scene_aspectmode='cube'):
@@ -138,13 +138,17 @@ def main():
     if 'node_radius_size' not in st.session_state:
         st.session_state.node_radius_size = 50
     if 'graph_type' not in st.session_state:
-        st.session_state.graph_type = '3D Scatter'
+        st.session_state.graph_type = 'Scatter'
     if 'node_symbol' not in st.session_state:
         st.session_state.node_symbol = 'circle'
     if 'node_position' not in st.session_state:
         st.session_state.node_position = False
     if 'scene_aspectmode' not in st.session_state:
         st.session_state.scene_aspectmode = 'cube'
+    if 'game_level' not in st.session_state:
+        st.session_state.game_level = 1
+    if 'show_area_data' not in st.session_state:
+        st.session_state.show_area_data = False
 
     # Sidebar header
     st.sidebar.header('Project IGI Graph Viewer')
@@ -158,20 +162,32 @@ def main():
         st.session_state.node_position = st.checkbox('Node Position', st.session_state.node_position)
         
     with st.sidebar.expander('View Settings',expanded=False):
-        st.session_state.show_table_data = st.checkbox('Table View', st.session_state.show_table_data)
+        st.session_state.show_table_data = st.checkbox('Node Table', st.session_state.show_table_data)
+        st.session_state.show_area_data = st.checkbox('Area Table', st.session_state.show_area_data)
         st.session_state.show_links = st.checkbox('Node Links', st.session_state.show_links)
         st.session_state.single_space = st.checkbox('Single Space', False)
         st.session_state.scene_aspectmode = st.selectbox('Aspect Mode', ['auto', 'cube', 'data', 'manual'], index=['auto', 'cube', 'data', 'manual'].index(st.session_state.scene_aspectmode))
     
-    with st.sidebar.expander('Node Settings',expanded=False):
+    with st.sidebar.expander('Graph & Node Settings',expanded=False):
         st.session_state.node_height = st.checkbox('Node Height', st.session_state.node_height)
         st.session_state.node_radius_size = st.slider("Node Radius Size:", 10, 100, st.session_state.node_radius_size)
-        st.session_state.graph_type = st.selectbox('Graph Type', ['3D Scatter', '3D Surface', '3D Line', '3D Mesh'], index=['3D Scatter', '3D Surface', '3D Line', '3D Mesh'].index(st.session_state.graph_type))
+        st.session_state.graph_type = st.selectbox('Graph Type', ['Scatter', 'Surface', 'Line', 'Mesh'], index=['Scatter', 'Surface', 'Line', 'Mesh'].index(st.session_state.graph_type))
         st.session_state.node_symbol = st.selectbox('Node Symbol', ['circle', 'circle-open', 'cross', 'diamond', 'diamond-open', 'square', 'square-open', 'x'], index=['circle', 'circle-open', 'cross', 'diamond', 'diamond-open', 'square', 'square-open', 'x'].index(st.session_state.node_symbol))
 
+    with st.sidebar.expander('Game Settings', expanded=False):
+        st.session_state.game_level = st.selectbox('Game Level', list(range(1, 15)), index=st.session_state.game_level - 1)
+        
     # File uploader
     uploaded_files = st.file_uploader('Upload Graph Files', type=['dat'], accept_multiple_files=True)
 
+    # Display the area data in a table if the checkbox is checked
+    if st.session_state.show_area_data:
+        area_json_data = GraphArea.get_json_data(st.session_state.game_level)
+        data = json.loads(area_json_data)
+        df = pd.DataFrame(data)
+        st.subheader(f"Level {st.session_state.game_level} Area Data")
+        st.dataframe(df)
+    
     all_data = []
 
     for uploaded_file in uploaded_files:
@@ -191,8 +207,18 @@ def main():
              # Display the data in a table if the checkbox is checked
                 if st.session_state.show_table_data:
                     df = pd.DataFrame(data)
-                    st.subheader('Graph Data')
+                    st.subheader(f"Graph # {uploaded_file.name.split('graph')[1].split('.')[0]} Data")
                     st.dataframe(df)
+                    
+            # Set the title of Plot and the page
+            graph_id = int(uploaded_file.name.split('graph')[1].split('.')[0])
+            graph_name = GraphArea.get_area_by_graph_id(st.session_state.game_level, graph_id) if GraphArea.get_area_by_graph_id(st.session_state.game_level, graph_id) else 'Unknown Area'
+            plot_name = 'Graph #' + str(graph_id) + '\t' + graph_name
+            # set this name to the title of plotting graph
+            st.title(plot_name)
+            # set title font size
+            st.markdown('<style>h1{font-size: 20px;}</style>', unsafe_allow_html=True)
+            
         finally:
             os.remove(uploaded_path)
 
--- libs/graph_area_parser.py
+++ libs/graph_area_parser.py
@@ -4,49 +4,61 @@ import requests
 class GraphArea:
     BASE_URL = "https://raw.githubusercontent.com/Jones-HM/IGI-Research-Data/main/Data/Graphs/Areas-JSON/graph_area_level{}.json"
 
-    def __init__(self, level):
-        self.level = level
-        self.data = self._fetch_data()
-
-    def _fetch_data(self):
-        response = requests.get(self.BASE_URL.format(self.level))
+    @staticmethod
+    def _fetch_data(level):
+        response = requests.get(GraphArea.BASE_URL.format(level))
         if response.status_code == 200:
             return response.json()
         else:
-            raise ValueError(f"Failed to fetch data for level {self.level}")
-    
-    def get_json_data(self):
-        # return the json with indentation of 4 spaces
-        return json.dumps(self.data, indent=4)
+            raise ValueError(f"Failed to fetch data for level {level}")
 
-    def get_csv_data(self):
+    @staticmethod
+    def get_json_data(level):
+        data = GraphArea._fetch_data(level)
+        return json.dumps(data, indent=4)
+
+    @staticmethod
+    def get_csv_data(level):
+        data = GraphArea._fetch_data(level)
         csv_data = []
-        for entry in self.data:
+        for entry in data:
             csv_data.append([entry["Area"], entry["Graph"]])
         return csv_data
-    
-    def get_area_by_graph(self, graph_name):
-        for entry in self.data:
+
+    @staticmethod
+    def get_area_by_graph(level, graph_name):
+        data = GraphArea._fetch_data(level)
+        for entry in data:
             if entry["Area"] == graph_name:
                 return entry["Graph"]
         return None
 
-    def get_all_areas(self):
-        return [entry["Area"] for entry in self.data]
+    @staticmethod
+    def get_all_areas(level):
+        data = GraphArea._fetch_data(level)
+        return [entry["Area"] for entry in data]
 
-    def get_all_graphs(self):
-        return [entry["Graph"] for entry in self.data]
+    @staticmethod
+    def get_all_graphs(level):
+        data = GraphArea._fetch_data(level)
+        return [entry["Graph"] for entry in data]
 
-    def get_area_count(self):
-        return len(self.data)
+    @staticmethod
+    def get_area_count(level):
+        data = GraphArea._fetch_data(level)
+        return len(data)
 
-    def get_all_areas_except_cutscene(self):
-        return [entry["Area"] for entry in self.data if entry["Area"] != "Cutscene Area"]
-    
-    def get_area_by_graph_id(self, graph_id):
+    @staticmethod
+    def get_all_areas_except_cutscene(level):
+        data = GraphArea._fetch_data(level)
+        return [entry["Area"] for entry in data if entry["Area"] != "Cutscene Area"]
+
+    @staticmethod
+    def get_area_by_graph_id(level, graph_id):
+        data = GraphArea._fetch_data(level)
         try:
             graph_name = f"Graph #{graph_id}"
-            for entry in self.data:
+            for entry in data:
                 if entry["Graph"] == graph_name:
                     return entry["Area"]
             return None
